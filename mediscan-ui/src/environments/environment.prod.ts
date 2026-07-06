c882c1d2e1bd340356f4c6a6f7c2c89642781b90
Shreya <shreyabalaji123@gmail.com>
2026-07-06 11:56:09 -0500

    Point production build at the deployed Render API URL

--- mediscan-ui/src/environments/environment.prod.ts
+++ mediscan-ui/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-// TODO: replace with your deployed Render API URL once it exists,
-// e.g. 'https://mediscan-api.onrender.com/api'
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://mediscan-api.onrender.com/api'
+  apiBaseUrl: 'https://mediscan-api-611u.onrender.com/api'
 };
